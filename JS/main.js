c0002906e6436cac816616a270f2d52362ccb56c
nilsonamaral <nilsonamaral7@gmail.com>
2022-07-17 20:57:14 -0300

    estou na metade da aula de listas

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,49 @@
-// capturar subimt do formulário    
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnTarefa = document.querySelector('.btn-tarefa');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+inputTarefa.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value)
+    }
+});
+
+function criaBotaoApagar(li) {
+    li.innerText += " ";
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = "Apagar";
+    botaoApagar.setAttribute('class', 'apagar');
+    botaoApagar.setAttribute('title', 'apagar essa tarefa')
+    li.appendChild(botaoApagar);
+}
+
+function limpaInput() {
+    inputTarefa.value = "";
+    inputTarefa.focus();
+}
+
+function criaTarefa(textoInput) {
+    const li = criaLi();
+    li.innerText = textoInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+}
+
+btnTarefa.addEventListener('click', function() {
+ if (!inputTarefa.value) return;
+ criaTarefa(inputTarefa.value)
+});
+
+document.addEventListener('click', function(e) {
+    const el = e.target;
+    if (el.classList.contains('apagar')) {
+    console.log('apagar clicado'); 
+}
+});
